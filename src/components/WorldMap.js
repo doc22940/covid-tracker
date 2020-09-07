21740bae052903f24afa376ff5ad0ce7266b12f7
Yogesh kumar <yogeshkumar87@hotmail.com>
2020-09-07 11:28:08 +0530

    styled map component

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import { VectorMap } from '@south-paw/react-vector-maps';
+import { makeStyles } from '@material-ui/core/styles';
 
 import world from '../helpers/world.json';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(5),
+        '& > svg': {
+            stroke: '#fff',
+            '& > path': {
+                // fill: '#a82b2b',
+                fill: '#EE2677',
+                cursor: 'pointer',
+                outline: 'none',
+
+                // When a layer is hovered
+                '&:hover': {
+                    fill: '#EBB3A9',
+                },
+
+                // When a layer is focused.
+                '&:focus': {
+                    fill: '#E86252',
+                },
+            },
+        },
+    },
+}));
+
 function WorldMap() {
+    const styles = useStyles();
+
     return (
-        <div style={{ padding: '2rem' }}>
+        <div className={styles.root}>
             <VectorMap {...world} />
         </div>
     );
