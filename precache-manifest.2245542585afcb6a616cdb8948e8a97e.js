f37e844dc38c0e939aaf8aa915e1839565ffd9e4
Yogesh kumar <yogeshkumar87@hotmail.com>
2020-10-22 19:02:48 +0530

    Updates

---
+++ precache-manifest.2245542585afcb6a616cdb8948e8a97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc3f3b4e5a0ef0f04ca73ca6bf0f7a0",
+    "url": "/yogiindia/covid-tracker/index.html"
+  },
+  {
+    "revision": "a3c186d9f65a24cf1b66",
+    "url": "/yogiindia/covid-tracker/static/css/main.68440b72.chunk.css"
+  },
+  {
+    "revision": "80d8e06a6d78a2768190",
+    "url": "/yogiindia/covid-tracker/static/js/2.0470cef1.chunk.js"
+  },
+  {
+    "revision": "b12b82fabf11b89ce79d62e1cc231b7e",
+    "url": "/yogiindia/covid-tracker/static/js/2.0470cef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c186d9f65a24cf1b66",
+    "url": "/yogiindia/covid-tracker/static/js/main.1d6b5e4a.chunk.js"
+  },
+  {
+    "revision": "399a13f3d6458e110574",
+    "url": "/yogiindia/covid-tracker/static/js/runtime-main.2f167297.js"
+  }
+]);
